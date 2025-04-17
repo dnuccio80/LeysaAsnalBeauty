0116271475d8c8e152a35a287f85f13149462f2f
dnuccio80 <nuccio227@gmail.com>
2025-04-17 11:45:24 -0300

    - UI modifications on ClientItem:   * Box Container Color in darkGray   * added the capital first name letter in uppercase

--- app/src/main/java/com/example/leysaasnalbeauty/leyasnal/ui/components/ClientItem.kt
+++ app/src/main/java/com/example/leysaasnalbeauty/leyasnal/ui/components/ClientItem.kt
@@ -10,9 +10,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.leysaasnalbeauty.leyasnal.ui.dataclasses.ClientDataClass
 
@@ -26,10 +28,13 @@ fun ClientItem(client: ClientDataClass) {
     ) {
         Card(
             modifier = Modifier.size(40.dp),
-            shape = CircleShape
+            shape = CircleShape,
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
         ) {
             Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-                SecondTitleText("L")
+                SecondTitleText(client.name.first().toString().uppercase())
             }
         }
         Column(Modifier.fillMaxWidth()) {
